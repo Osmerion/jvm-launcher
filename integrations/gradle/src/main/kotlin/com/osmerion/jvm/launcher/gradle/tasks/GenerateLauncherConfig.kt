13266a95d73680919c9a77685823529a63fe62b4
Leon Linhart <themrmilchmann@gmail.com>
2025-04-14 16:56:53 +0200

    chore: remove unused import

--- integrations/gradle/src/main/kotlin/com/osmerion/jvm/launcher/gradle/tasks/GenerateLauncherConfig.kt
+++ integrations/gradle/src/main/kotlin/com/osmerion/jvm/launcher/gradle/tasks/GenerateLauncherConfig.kt
@@ -23,7 +23,6 @@ import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
-import javax.inject.Inject
 
 /**
  * Generates the configuration file for a launcher executable.
